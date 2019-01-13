7961be0595952e00f48c0c28a051ca3ca5dee047
Mishin Alexey <mishin-lex@mail.ru>
2019-01-13 16:21:50 +0300

    Add migration, model and seeder for miltary districts

---
+++ app/MiltaryDistrict.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MiltaryDistrict extends Model
+{
+    //
+    protected $table = 'miltary_districts';
+}
---
+++ database/seeds/MiltaryDistrictsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MiltaryDistrictsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $districts = [
+            'Западный военный округ',
+            'Южный военный округ',
+            'Северный морской флот',
+            'Центральный военный округ',
+            'Восточный военный округ',
+        ];
+
+        foreach ($districts as $district_name) {
+            DB::table('miltary_districts')->insert([
+                'name' => $district_name
+            ]);
+        }
+    }
+}
