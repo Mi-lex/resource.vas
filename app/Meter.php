93a7e82cbbfc1a120ed26e2f9d294f85820b30d7
Mishin Alexey <mishin-lex@mail.ru>
2019-01-21 16:40:36 +0300

    Add consumptions methods in meter model

--- app/Meter.php
+++ app/Meter.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meter extends Model
 {
-    //
+    public function electricity_consumptions()
+    {
+        return $this->hasMany('App\ElectricityConsumption', 'device_id');
+    }
+
+    public function water_consumptions()
+    {
+        return $this->hasMany('App\WaterConsumption', 'device_id');
+    }
+
+    public function heat_consumptions()
+    {
+        return $this->hasMany('App\heatConsumption', 'device_id');
+    }
 }
