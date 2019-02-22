1a92b451ccfac58f950fa70e894825a7411a8de1
Mishin Alexey <mishin-lex@mail.ru>
2019-02-22 22:12:44 +0300

    Fix water consumptions seeder

--- database/seeds/WaterConsumptionsTableSeeder.php
+++ database/seeds/WaterConsumptionsTableSeeder.php
@@ -13,14 +13,15 @@ class WaterConsumptionsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $start_date = Carbon::createFromTimeStamp($faker
-            ->dateTimeBetween($startDate = '-30 days', $endDate = '-29 days')
-            ->getTimestamp());
-        $records_amount = 12;
-        $active_water_meters = [7, 17, 19, 21, 23];
+        $start_date = Carbon::now()->subDays(40);
+        
+        $records_amount = 961;
+        // $active_water_meters = [7, 17, 19, 21, 23];
+        $active_water_meters = [7, 17];
 
         foreach ($active_water_meters as $device_id) {
             $consumption_amount = $faker->randomFloat(1, 140000, 2);
+            $start_date = Carbon::now()->subDays(40);
 
             foreach (range(1, $records_amount) as $outer_index) {
                 DB::table('water_consumptions')->insert([
@@ -28,9 +29,9 @@ class WaterConsumptionsTableSeeder extends Seeder
                     'created_at' => $start_date,
                     'consumption_amount' => $consumption_amount += $faker->randomFloat(1, 100, 200)
                 ]);
-            }
 
-            $start_date->addHour();
+                $start_date->addHour();
+            }
         }
     }
 }
