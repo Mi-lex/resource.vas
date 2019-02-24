06bc74846dd886b1f9fb00d68e8fbaa8f7a50ab4
Mishin Alexey <mishin-lex@mail.ru>
2019-02-24 11:03:10 +0300

    Add ability to get water and electricity consumptions of miltary objects

--- app/MiltaryObject.php
+++ app/MiltaryObject.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class MiltaryObject extends Model
 {
-    //
+    public function buildings()
+    {
+        return $this->hasMany('App\Building', 'object_id');    
+    }
+
+    public function meters_count()
+    {
+        return $this->buildings()->withCount('meters')->get()->sum('meters_count');  
+    }
+
+    public function water_consumption()
+    {
+        $sum = $this->buildings->sum(function ($building) {
+            return $building->water_consumption();
+        });
+
+        return $sum;
+    }
+
+    public function electricity_consumption()
+    {
+        $sum = $this->buildings->sum(function ($building) {
+            return $building->electricity_consumption();
+        });
+
+        return $sum;
+    }
 }
