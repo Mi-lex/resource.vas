38be8437d6d370d3805981888d6c7e68168d5f6d
= <=>
2019-03-12 18:24:37 +0300

    Add socket class for connecting to devices

---
+++ app/Socket/Socket.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Socket;
+
+use Illuminate\Support\Facades\Log;
+
+class Socket {
+    private $connection;
+    private $errno;
+    private $error_message;
+
+    public function __construct($connection_params)
+    {
+        extract($connection_params);
+
+        $this->connection = 
+            stream_socket_client("$protocol://$ip:$port", $this->errno, $this->error_message, 3);
+    }
+
+    public function get_answer($command)
+    {
+        $result = false;
+
+        if (!$this->connection) {
+            Log::error("Ошибка соединения: $this->errno $this->error_message");
+        } else {
+            fwrite($this->connection, $command);
+
+            $result = fread($this->connection, 8192);
+        }
+        
+        fclose($this->connection);
+
+        return $result;
+    }
+}
