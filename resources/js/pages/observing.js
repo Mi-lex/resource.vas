585dd394e1f43f1dcb4ea7e09414c369fb9c289d
Milex <mishin-lex@mail.ru>
2019-10-02 11:43:22 +0300

    Implement client rendering of buildings with meters

---
+++ resources/js/pages/observing.js
@@ -0,0 +1,145 @@
+// Для начала нужно получить на странице полный список зданий с устройствами в них
+// Пройтись по ним массивом и зарендерить все согласно шаблону
+import app_constants from "../constants";
+const { base_url } = app_constants;
+
+const iconNames = {
+    'electricity': 'flash',
+    'water': 'tint',
+    'heat': 'fire'
+}
+
+function getElementFromTemplate(templateString, containerElement = { tag: 'div', className: '' }) {
+    const container = document.createElement(containerElement.tag);
+    container.className = containerElement.className;
+
+    container.innerHTML = templateString;
+
+    return container;
+}
+
+class Meter {
+    constructor(meter) {
+        this.id = meter.id
+        this.name = meter.name;
+        this.type = meter.typeName;
+    }
+
+    get template() {
+        return `
+            <li class="obs-devices__item" id=meter_id_${this.id}>
+                <a class="obs-devices__item-icon" href=${base_url + 'meters/' + this.id}>
+                    <i style="color: #84DBFF;" class="fa fa-${iconNames[this.type]}"></i>
+                </a>
+            </li>
+        `.trim();
+    }
+}
+
+class Building {
+    constructor(building) {
+        this.id = building.id
+        this.name = building.short_name;
+        this.meters = building.meters_arr.map(meter => new Meter(meter));
+    }
+
+    get template() {
+        return `
+            <li class="obs-building__item" id=building_id_${this.id}>
+                <header class="obs-building__item-header">
+                    <a class="obs-building__item-title" href=${base_url + 'buildings/' + this.id}>
+                        ${this.name}
+                    </a>
+                    <svg width="31" height="31" class="page-header__icon page-header__icon--search">
+                        <use xlink:href="img/icons/sprite.svg#building-icon"></use>
+                    </svg>
+                </header>
+                <div class="obs-building__item-body">
+                    <ul class="obs-devices__list">
+                        ${this.meters.map(meter => meter.template).join(``)}
+                    </ul>
+                </div>
+            </li>`.trim();
+    }
+};
+
+class BuildingList {
+    constructor() {
+        this.buildingListUrl = `${base_url}/building_list`;
+        this.meterValuesUrl = `${base_url}/meters/values`;
+    }
+
+    loadSpinner() {
+        return null;
+    }
+
+    renderBuildings() {
+        const template = this.buildingList.map(building => building.template)
+            .join(``).trim();
+
+        const container = {
+            tag: 'ul',
+            className: 'obs-building__list'
+        };
+
+        this.domList = getElementFromTemplate(template, container);
+
+        const wrapper = document.querySelector('.content-wrapper');
+        wrapper.appendChild(this.domList);
+    }
+
+    showElemenet() {
+        $.ajax({
+            url: this.buildingListUrl,
+            type: 'GET',
+            dataType: 'json',
+            beforeSend: this.loadSpinner
+        })
+            .done(buildingsData => {
+                this.buildingList = buildingsData
+                    .map(buildingData => new Building(buildingData));
+                this.renderBuildings();
+                this.showActiveness();
+            })
+            .fail(function (data) {
+                console.log(this.base_url)
+                console.log(data.responseText);
+            });
+    }
+
+    showActiveness() {
+        $.ajax({
+            url: this.meterValuesUrl,
+            type: 'GET',
+            dataType: 'json',
+        })
+            .done(valuesData => {
+                this.meterValues = valuesData;
+                this.insertMeterClasses();
+            })
+            .fail(function (data) {
+                console.log(data.responseText);
+            });
+    }
+
+
+    insertMeterClasses() {
+        this.meterValues.forEach(el => {
+            const domMeter = document.getElementById(`meter_id_${el.meter_id}`);
+
+            let statusStr = 'obs-devices__item--';
+            statusStr += el.meter_value ? 'active' : 'broken';
+
+            domMeter.classList.add(statusStr);
+        });
+    }
+
+    get elements() {
+        return this.buildingList;
+    }
+}
+
+const app = new BuildingList();
+app.showElemenet();
+
+
