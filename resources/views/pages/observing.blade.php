a0a8b9dd4a8052ff087444f3c6fa63eeeddc9c0e
Milex <mishin-lex@mail.ru>
2019-10-16 09:14:23 +0300

    Write html and styles for alert popup

--- resources/views/pages/observing.blade.php
+++ resources/views/pages/observing.blade.php
@@ -18,6 +18,26 @@
         </form>
         <button class="obs-start-monitoring-btn">Начать мониторинг</button>
     </section>
+    <div class="obs-alert-popup__overlay" style="display: none;">
+        <div class="obs-alert-popup__container">
+            <section class="obs-alert-popup">
+                <div class="obs-alert-popup__upper-part">
+                    <button class="obs-alert-popup__close-btn">
+                        ✕
+                    </button>
+                </div>
+                
+                <div class="obs-alert-popup__body">
+                    <strong class="obs-alert-popup__message">Внимание!</strong>
+                    <p class="obs-alert-popup__text">
+                        Обнаружено подозрительное потребление в приборе учета 
+                        <a href="/meters/3">Цод основной ввод</a>, 
+                        расположенном в <a href="/buildings/4">ГП-4</a>.
+                    </p>
+                </div>
+            </section>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
